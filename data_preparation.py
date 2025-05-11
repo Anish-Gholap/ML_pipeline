78abc67b12560031ac3dae06809ff55b9ba33276
Anish-Gholap <anishgholap@gmail.com>
2025-05-11 19:31:34 +0800

    Pipeline code from notebook

---
+++ data_preparation.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+from sklearn.impute import SimpleImputer
+
+from data_collection import load_housing_data
+from sklearn.model_selection import train_test_split
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+
+def prepare_data():
+    '''
+    1. Add the income_cat column
+    2. Stratify split
+    3. Preprocess splits
+    4. Return splits
+    '''
+
+    df = load_housing_data()
+
+    df["income_cat"] = pd.cut(df["median_income"], bins=[0, 1.5, 3.0, 4.5, 6, np.inf], labels=[1, 2, 3, 4, 5])
+    strat_train_set, strat_test_set = train_test_split(df, test_size=0.2, stratify=df["income_cat"], random_state=42)
+
+    for set_ in (strat_train_set, strat_test_set):
+        set_.drop("income_cat", axis=1, inplace=True)
+
+    X_train = strat_train_set.drop("median_house_value", axis = 1)
+    y_train = strat_train_set["median_house_value"].copy()
+
+    X_test = strat_test_set.drop("median_house_value", axis=1)
+    y_test = strat_test_set["median_house_value"].copy()
+
+    return X_train, y_train, X_test, y_test
---
+++ model.py
@@ -0,0 +1,140 @@
+import joblib
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.compose import ColumnTransformer, make_column_selector
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, FunctionTransformer
+from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.metrics.pairwise import rbf_kernel
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import randint
+from data_preparation import prepare_data
+
+def build_model():
+    '''
+    Trains the model and saves it
+
+    1. Load the splits
+    2. Preprocess the training data
+    3. Evaluate model
+    4. Train the model
+    5. Save the model
+    '''
+
+    # 1. Load the splits
+    X_train, y_train, X_test, y_test = prepare_data()
+
+    # 2. Preprocess Training Data
+    preprocessing = make_preprocessing_pipeline()
+
+    # 3. Train the model
+    model = train_model(X_train, y_train, preprocessing)
+
+    # 4. Evaluate the model
+    evaluate_model(X_test, y_test, model)
+
+    # 5. Save the model
+    save_model(model)
+
+
+
+
+def column_ratio(X):
+    return X[:, [0]] / X[:, [1]]
+
+def ratio_name(function_transformer, feature_names_in):
+    return ["ratio"] # feature names out
+
+def ratio_pipeline():
+    return make_pipeline(
+        SimpleImputer(strategy="median"),
+        FunctionTransformer(column_ratio,
+        feature_names_out=ratio_name),
+        StandardScaler()
+    )
+
+
+class ClusterSimilarity(BaseEstimator, TransformerMixin):
+    def __init__(self, n_clusters=10, gamma=1.0,
+        random_state=None):
+        self.n_clusters = n_clusters
+        self.gamma = gamma
+        self.random_state = random_state
+
+    def fit(self, X, y=None, sample_weight=None):
+        self.kmeans_ = KMeans(self.n_clusters,
+        random_state=self.random_state)
+        self.kmeans_.fit(X, sample_weight=sample_weight)
+        return self # always return self!
+
+    def transform(self, X):
+        return rbf_kernel(X, self.kmeans_.cluster_centers_,
+            gamma=self.gamma)
+
+    def get_feature_names_out(self, names=None):
+        return [f"Cluster {i} similarity" for i in
+            range(self.n_clusters)]
+
+def make_preprocessing_pipeline():
+    log_pipeline = make_pipeline(
+        SimpleImputer(strategy="median"),
+        FunctionTransformer(np.log, feature_names_out="one-to-one"),
+        StandardScaler()
+    )
+
+    cluster_simil = ClusterSimilarity(n_clusters=10, gamma=1., random_state=42)
+
+    default_num_pipeline = make_pipeline(
+        SimpleImputer(strategy="median"),
+        StandardScaler()
+    )
+
+    cat_pipeline = make_pipeline(
+        SimpleImputer(strategy="most_frequent"),
+        OneHotEncoder(handle_unknown="ignore"))
+
+    preprocessing = ColumnTransformer([
+        ("bedrooms", ratio_pipeline(), ["total_bedrooms", "total_rooms"]),
+        ("rooms_per_house", ratio_pipeline(), ["total_rooms", "households"]),
+        ("people_per_house", ratio_pipeline(), ["population", "households"]),
+        ("log", log_pipeline, ["total_bedrooms", "total_rooms", "population", "households", "median_income"]),
+        ("geo", cluster_simil, ["latitude", "longitude"]),
+        ("cat", cat_pipeline, make_column_selector(dtype_include=object)),
+    ], remainder=default_num_pipeline)  # one column remaining housing_median_age
+
+    return preprocessing
+
+def train_model(X_train, y_train, preprocessing):
+    full_pipeline = Pipeline([
+        ("preprocessing", preprocessing),
+        ('random_forest', RandomForestRegressor(random_state=42))
+    ])
+    params_distribs = {
+        'preprocessing__geo__n_clusters': randint(low=3, high=50),
+        'random_forest__max_features': randint(low=2, high=20)
+    }
+
+    random_search = RandomizedSearchCV(
+        full_pipeline, params_distribs, n_iter=1, cv=3, scoring='neg_root_mean_squared_error', random_state=42
+    )
+
+    random_search.fit(X_train, y_train)
+
+    model = random_search.best_estimator_ # includes preprocessing
+
+    return model
+
+def evaluate_model(X_test, y_test, model):
+    preds = model.predict(X_test)
+    mse = mean_squared_error(y_test, preds)
+    print(f'RMSE: {np.sqrt(mse)}')
+
+def save_model(model):
+    joblib.dump(model, "models/housing_model.pkl")
+
+
+# Test
+build_model()
